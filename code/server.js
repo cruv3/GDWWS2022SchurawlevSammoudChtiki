092f51687551980d454d216e05dffede808d2b3e
Mayess Sammoud <mayesssammoud@nat079007.nat.FH-Koeln.DE>
2021-12-08 15:41:24 +0100

    PoC

--- code/server.js
+++ code/server.js
@@ -1,7 +1,9 @@
-import fetch from 'node-fetch';
+const fs = require("fs")
+const fetch = require("node-fetch");
 
+getProduct("chocolate")
 
-
+async function getProduct(name){
 //Open Exchange Rate API
 // API KEY: d7cafdd25e7d4b7193244ccf6e610329
 let open_exchange_rate_api_key = "d7cafdd25e7d4b7193244ccf6e610329"
@@ -11,9 +13,30 @@ fetch(open_exchange_rate_api_request)
     .then((response) => {
         return response.json();
     })
-    .then((myJson) => {
-        //Alle Rates zu USD 
-        console.log(myJson);
-        // UST zu EUR 
-        console.log(myJson.rates.EUR);
-    });
+    .then((ratesJson) => {
+        //Alle Rates zu USD
+        console.log(ratesJson);
+        // USD zu EUR
+        console.log("USD EUR Rate: " + ratesJson.rates.EUR);
+        return ratesJson.rates.EUR
+    }).then((rate)=>{
+        
+        fs.readFile("prices.json", (err, data) => {
+            if (err) throw (err)
+            obj = JSON.parse(data)
+            let result = []
+            obj.forEach(element => {
+                if (element.product.toUpperCase().includes(name.toUpperCase())){
+                    console.log(element)
+                    var price = element.price.substring(1)
+                    var newPrice = (parseFloat(price) * rate).toFixed(2)
+                    element.price= "€" + newPrice
+                    result.push(element)
+                    console.log(element)
+                }
+            })
+        })
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
