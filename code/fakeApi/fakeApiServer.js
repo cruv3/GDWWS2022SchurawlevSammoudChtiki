c173b6ab5abe7f5f88f7f914de6df9448c95403d
Andi <andreasschurawlev@Andreass-Air.fritz.box>
2022-01-16 14:43:41 +0100

    noch was

--- code/fakeApi/fakeApiServer.js
+++ code/fakeApi/fakeApiServer.js
@@ -7,7 +7,6 @@ dotenv.config()
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
 const productRoutes = require('./resources/products')
